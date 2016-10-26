39c9c0b801422ec1dbef87ee9aa71a2c34ca9e17
Mano Marks <mano.marks@gmail.com>
2016-10-26 15:50:03 -0700

    Update iis generate Added new WindowsOptionalFeatures to comply with needed features to add a webste. Fixed a number of problems with the `RUN` commands that were causing issues like badly parsed strings etc. on `docker build`.

--- Artifacts/IIS/Generate.ps1
+++ Artifacts/IIS/Generate.ps1
@@ -21,14 +21,14 @@ $Artifact = Get-Content -Path $Manifest -Raw | ConvertFrom-Json
 if ($Artifact.Status -eq 'Present') {
     $Result = '
 RUN powershell.exe -ExecutionPolicy Bypass -Command \ 
-    Enable-WindowsOptionalFeature -Online -FeatureName Web-Server, IIS-WebServerManagementTools; \
-'
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebserverRole, IIS-WebServer, IIS-WebServerManagementTools;'
     $Add = "`r`n"
     [int]$SiteCount = $Artifact.Websites.name.Length;
     for ($i=0;$i -lt $SiteCount;$i++){
         Write-Verbose -Message ('Creating new website for {0} site' -f $Artifact.Websites.Name[$i])
         $SitePath = ($Artifact.Websites.MountPath + '\' + (Split-Path $Artifact.Websites.PhysicalPath[$i] -NoQualifier)) -replace '\\%SystemDrive%\\', "\"
-        $Result += 'New-Website -Name "{0}" -PhysicalPath "{1}" \{2}' -f $Artifact.Websites.Name[$i], $Artifact.Websites.PhysicalPath[$i], "`r`n"
+        $Result += '\{1}New-Item -Path {0} -ItemType directory; \{1}' -f $Artifact.Websites.PhysicalPath[$i], "`r`n"
+        $Result += 'New-Website -Name ''{0}'' -PhysicalPath "{1}";' -f ($Artifact.Websites.Name[$i] -replace "'","''"), $Artifact.Websites.PhysicalPath[$i], "`r`n"
         Write-Verbose -Message ('Copying files for {0} site' -f $Artifact.Websites.Name[$i])
         Copy-Item $SitePath $ManifestPath
         $Add += "ADD {0} {1}`r`n" -f (Split-Path $Artifact.Websites.PhysicalPath[$i] -Leaf),$Artifact.Websites.PhysicalPath[$i] 
