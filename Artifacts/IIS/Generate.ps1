4b3a7c27429a2a403331f81af7ac7968eb94fac5
Mano Marks <mano.marks@gmail.com>
2016-10-26 17:39:04 -0700

    Added support for copying iis configuration files

--- Artifacts/IIS/Generate.ps1
+++ Artifacts/IIS/Generate.ps1
@@ -19,6 +19,9 @@ $Manifest = '{0}\{1}.json' -f $ManifestPath, $ArtifactName
 $Artifact = Get-Content -Path $Manifest -Raw | ConvertFrom-Json
 
 if ($Artifact.Status -eq 'Present') {
+    Write-Verbose ('Copying {0} configuration files' -f $ArtifactName)
+    $ConfigPath = $Artifact.Websites.MountPath + "\" + "Windows\System32\inetsrv\config"
+    Copy-Item $ConfigPath $ManifestPath -Recurse
     $Result = '
 RUN powershell.exe -ExecutionPolicy Bypass -Command \ 
     Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebserverRole, IIS-WebServer, IIS-WebServerManagementTools;'
@@ -30,15 +33,15 @@ RUN powershell.exe -ExecutionPolicy Bypass -Command \
         $Result += '\{1}New-Item -Path {0} -ItemType directory; \{1}' -f $Artifact.Websites.PhysicalPath[$i], "`r`n"
         $Result += 'New-Website -Name ''{0}'' -PhysicalPath "{1}";' -f ($Artifact.Websites.Name[$i] -replace "'","''"), $Artifact.Websites.PhysicalPath[$i], "`r`n"
         Write-Verbose -Message ('Copying files for {0} site' -f $Artifact.Websites.Name[$i])
-        Copy-Item $SitePath $ManifestPath
-        $Add += "ADD {0} {1}`r`n" -f (Split-Path $Artifact.Websites.PhysicalPath[$i] -Leaf),$Artifact.Websites.PhysicalPath[$i] 
+        Copy-Item $SitePath $ManifestPath -Recurse
+        $Add += "ADD {0} {1}`r`n" -f (Split-Path $Artifact.Websites.PhysicalPath[$i] -Leaf),($Artifact.Websites.PhysicalPath[$i] -Replace "\\","/") 
     }
 
     ### Add IIS HTTP handlers to the Dockerfile
     foreach ($HttpHandler in $Artifact.HttpHandlers) {
         $Result += 'New-WebHandler -Name "{0}" -Path "{1}" -Verb "{2}" \{3}' -f $HttpHandler.Name, $HttpHandler.Path, $HttpHandler.Verb, "`r`n" 
     }
-
+    $Add += "Add config Windows/System32/inetsrv/"
     Write-Output -InputObject ($Result + $Add)
     
 }
