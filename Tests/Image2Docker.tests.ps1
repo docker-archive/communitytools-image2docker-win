8ff2e36eeabea92650683fdf9478f632f343a6d2
Ryan Yates <ryan.yates@kilasuit.org>
2016-11-30 11:56:28 +0000

    For #11 - added appveyor.yml

--- Tests/Image2Docker.tests.ps1
+++ Tests/Image2Docker.tests.ps1
@@ -1,35 +1,43 @@
-describe Image2Docker {
-    $ModuleName = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Leaf
-    Write-Verbose -Message ('Module name is {0}' -f $ModuleName)
+Describe Image2Docker {
+    $ModuleName = 'Image2Docker'
+    WrIte-Verbose -Message ('Module name is {0}' -f $ModuleName)
 
-    ### Remove the module if it's already imported, then re-import it (in case code changed)
+    ### Remove the module if It's already imported, then re-import It (in case code changed)
     Remove-Module -Name $ModuleName -ErrorAction Ignore
-    #Import-Module -Name (Split-Path -Path $PSScriptRoot -Parent)
+    #Import-Module -Name (SplIt-Path -Path $PSScriptRoot -Parent)
     Import-Module -Name $PSScriptRoot\..\Image2Docker.psd1
 
-    context 'Public-facing PowerShell commands' {
-        it 'Has a ConvertTo-Dockerfile command' {
+    Context 'Public-facing PowerShell commands' {
+        It 'Has a ConvertTo-Dockerfile command' {
             (Get-Command -Module $ModuleName -Name ConvertTo-Dockerfile).Count | Should Be 1
         }
 
-        it 'Has a Get-WindowsArtifacts command' {
+        It 'Has a Get-WindowsArtifacts command' {
             (Get-Command -Module $ModuleName -Name Get-WindowsArtifacts).Count | Should Be 1
         }
     }
 
-    context 'Private / internal PowerShell commands' {
+    Context 'Private / internal PowerShell commands' {
         InModuleScope -ModuleName $ModuleName -ScriptBlock {
-            it 'Has a GenerateDockerfile command' {
+            It 'Has a GenerateDockerfile command' {
                 (Get-Command -Module $ModuleName -Name GenerateDockerfile).Count | Should Be 1
             }
-            it 'Has a GetImageType command' {
+            It 'Has a GetImageType command' {
                 (Get-Command -Module $ModuleName -Name GetImageType).Count | Should Be 1
             }
         }
     }
 
-    context 'Test ConvertTo-Dockerfile command' {
-        it 'Should throw when -ImagePath parameter value is invalid' {
+    Context 'Artifact Discovery' {
+            foreach ($artifact in Get-WindowsArtifacts) {
+            
+            It "Has an $artifact Artifact" {
+                Get-WindowsArtifacts | Where-Object {$_ -match $artifact }| Should Match $artifact
+            }
+      }
+  }
+     Context 'Test ConvertTo-Dockerfile command' {
+        It 'Should throw when -ImagePath parameter value is invalid' {
             { ConvertTo-Dockerfile -ImagePath c:\invalid\path.wim } | Should throw
         }
     }
