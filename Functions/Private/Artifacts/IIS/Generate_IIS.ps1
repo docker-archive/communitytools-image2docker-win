2e6b2fd17990bebdb3bc310b9dd4368bce3e5bd4
Thomas Purrer <thomas.purrer@techtalk.at>
2017-08-25 11:36:17 +0200

    fix usage of ProcessDirectory

--- Functions/Private/Artifacts/IIS/Generate_IIS.ps1
+++ Functions/Private/Artifacts/IIS/Generate_IIS.ps1
@@ -163,7 +163,7 @@ if ($Artifact.Status -eq 'Present') {
                 $newApp = "    New-WebApplication -Name '$appName' -Site '$($Site.Name)' -PhysicalPath 'C:$targetPath' -Force; ``"
                 $null = $AppBuilder.AppendLine($newApp)                
                 if ($sourcePath -ne $mainVirtualDir.PhysicalPath) {
-                    ProcessDirectory -DirectoryBuilder $DirectoryBuilder -CopyBuilder $CopyBuilder -SourcePath $sourcePath                  
+                    ProcessDirectory -DirectoryBuilder $DirectoryBuilder -CopyBuilder $CopyBuilder -AclBuilder @AclBuilder -SourcePath $sourcePath                  
                 }
             }
 
@@ -190,7 +190,7 @@ if ($Artifact.Status -eq 'Present') {
                 $null = $AppBuilder.AppendLine($newDir)
 
                 if ($sourcePath -ne $mainVirtualDir.PhysicalPath) {           
-                    ProcessDirectory -DirectoryBuilder $DirectoryBuilder -CopyBuilder $CopyBuilder -SourcePath $sourcePath
+                    ProcessDirectory -DirectoryBuilder $DirectoryBuilder -CopyBuilder $CopyBuilder -AclBuilder @AclBuilder -SourcePath $sourcePath
                 }
             }
         }      
